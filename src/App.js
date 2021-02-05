5140e67dc4089116134f38fb33e84739bdc790b0
Vlad <eyeseeice@gmail.com>
2021-02-05 11:25:16 +0300

    Initial source

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import {Switch, Router, Route, Redirect} from "react-router-dom";
+import MainPage from "./components/MainPage";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Workspace from "./components/Workspace";
+import Steep1 from "./components/Steep1";
+import Steep2 from "./components/Steep2";
+import Settings from "./components/Settings";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [redirect, setRedirect] = useState()
+    return (
+        <div>
+            {/*<Redirect to={redirect} />*/}
+            <Switch>
+                <Route exact path={'/'} component={() => <MainPage setRedirect={setRedirect} />}/>
+                <Route exact path={'/login'} render={() => <Login setRedirect={setRedirect}/>}/>
+                <Route exact path={'/signup'} render={() => <SignUp setRedirect={setRedirect}/>}/>
+                <Route exact path={'/steep1'} render={() => <Steep1 setRedirect={setRedirect}/>}/>
+                <Route exact path={'/steep2'} render={() => <Steep2 setRedirect={setRedirect}/>}/>
+                <Route exact path={'/settings'} render={() => <Settings setRedirect={setRedirect}/>}/>
+                <Route exact path={'/workspace'} render={() => <Workspace setRedirect={setRedirect}/>}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {render} from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {applyMiddleware, compose, createStore} from "redux";
+import {rootReducer} from "./redux/rootReducer";
+import {BrowserRouter} from "react-router-dom";
+import './index.css'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = createStore(rootReducer,composeWithDevTools(
+    applyMiddleware(thunk)
+))
+
+const app = () => {
+    return <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+}
+
+render(
+    app(),
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
